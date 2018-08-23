abc82b7586f16b01c42dab337a4164667670a870
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2018-08-23 20:39:42 -0300

    adicionado o banco de dados H2

---
+++ src/main/java/br/com/cursomvc/resources/CategoriaResource.java
@@ -0,0 +1,28 @@
+package br.com.cursomvc.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.cursomvc.model.Categoria;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+	
+	@GetMapping
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(1, "Escritório");
+		
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.add(cat1);
+		categorias.add(cat2);
+		
+		return categorias;
+	}
+}
