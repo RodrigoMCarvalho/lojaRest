13abc8b75067941f184a33fe0d380f2554705571
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2018-08-30 21:51:33 -0300

    paginação com parâmetros opcionais

--- src/main/java/br/com/cursomvc/services/CategoriaService.java
+++ src/main/java/br/com/cursomvc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.cursomvc.models.Categoria;
@@ -26,6 +29,12 @@ public class CategoriaService {
 		Optional<Categoria> categoria = repo.findById(id);
 		return categoria.orElseThrow(() -> new ObjectNotFoundException(
 						"Categoria não encontrada! ID " + id + ", do tipo: " + Categoria.class.getName()));
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage,String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
+				orderBy);
+		return repo.findAll(pageRequest);
 	}
 	
 	public Categoria save(Categoria categoria) {
