fb6c4f54e2b1d00bb596682f1b64f6b20580c129
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2018-10-06 22:04:07 -0300

    corrigindo esqueceu a senha

--- src/main/java/br/com/cursomvc/services/AbstractEmailService.java
+++ src/main/java/br/com/cursomvc/services/AbstractEmailService.java
@@ -91,7 +91,7 @@ public abstract class AbstractEmailService implements EmailService{
 		sm.setSubject("Solicitação de nova senha");
 		sm.setSentDate(new Date(System.currentTimeMillis()));
 		sm.setText("Nova senha: " + novaSenha);
-		return null;
+		return sm;
 	}
 	
 	
