c4017f7e0748db29afdbd2235d9b6f7cbd8ee504
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2018-11-19 20:40:09 -0200

    salvando arquivo na Amazon S3

--- src/main/java/br/com/cursomvc/CursomvcApplication.java
+++ src/main/java/br/com/cursomvc/CursomvcApplication.java
@@ -1,11 +1,17 @@
 package br.com.cursomvc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.cursomvc.services.S3Service;
+
 @SpringBootApplication
 public class CursomvcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	
 	public static void main(String[] args) {
@@ -14,7 +20,7 @@ public class CursomvcApplication implements CommandLineRunner {
 
 	@Override  //para iniciar dados num BD em memória
 	public void run(String... args) throws Exception {
-		
+		s3Service.uploadFile("C:\\Users\\rodri_000\\Desktop\\Desenvolvimento Web\\ws-ionic\\CursoSpringIonic\\src\\assets\\imgs\\cp1.jpg");
 	}
 	
 	
