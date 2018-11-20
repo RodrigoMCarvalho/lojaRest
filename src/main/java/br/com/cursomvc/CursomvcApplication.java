94d42c027154bfc2a6b5ee224b0f88cc89b42088
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2018-11-20 21:44:00 -0200

    realizando alterações no backend para visualizar imagens do bucket da Amazon S3

--- src/main/java/br/com/cursomvc/CursomvcApplication.java
+++ src/main/java/br/com/cursomvc/CursomvcApplication.java
@@ -1,17 +1,11 @@
 package br.com.cursomvc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.cursomvc.services.S3Service;
-
 @SpringBootApplication
 public class CursomvcApplication implements CommandLineRunner {
-	
-	@Autowired
-	private S3Service s3Service;
 	
 	
 	public static void main(String[] args) {
@@ -20,7 +14,6 @@ public class CursomvcApplication implements CommandLineRunner {
 
 	@Override  //para iniciar dados num BD em memória
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("C:\\Users\\rodri_000\\Desktop\\Desenvolvimento Web\\ws-ionic\\CursoSpringIonic\\src\\assets\\imgs\\cp1.jpg");
 	}
 	
 	
